213fceace62a7eb883d4f77b250bbe2820974e33
edmundmagnojr-nabepero <edmund.magno@nabepero.co.jp>
2022-12-13 17:32:28 +0800

    finished Section 24

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,19 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        //$request->session()->put(['edwin'=>'master instructor']);
+        //global sessions
+        //session(['peter'=>'student']);
+        //echo $request->session()->get('edwin');
+        //return $request->session()->all();
+        //return view('home');
+        //$request->session()->forget('edwin');
+        //$request->session()->flush();
+        //$request->session()->flash('message', 'Post has been created');
+        $request->session()->reflash();
+        $request->session()->keep('message');
+        return $request->session()->get('message');
     }
 }
